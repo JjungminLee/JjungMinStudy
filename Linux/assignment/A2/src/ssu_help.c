e4956040fef3e0e04cf8dc40e28ce297c7c78770
JjungminLee <julie0964@naver.com>
2024-02-21 12:33:50 +0900

    c딥다이브/메모리와 포인터

---
+++ Linux/assignment/A2/src/ssu_help.c
@@ -0,0 +1,6 @@
+
+
+int main(){
+    //fmd5 [FILE_EXTENSION] [MINSIZE] [MAXSIZE] [TARGET_DIRECTORY]
+    
+}
---
+++ Linux/assignment/A2/src/ssu_sdup.c
@@ -0,0 +1,8 @@
+#include "../includes/header.h";
+
+void prompt(){
+    printf("20221678>");
+}
+int main(){
+    // 명령어는 fmd5,fsha1,exit,help
+}
